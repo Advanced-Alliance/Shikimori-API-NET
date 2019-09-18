3a05cd37c0c93bf5dc6d425b3f2a93ebdedab93f
ShadowHD33RUS <shadowhd33rus@gmail.com>
2019-09-18 17:02:02 +0300

    #SU4NA-65: Создать новый репозиторий для библиотеки поддержки API Shikimori.one

---
+++ Shikimori-API-NET/ShikimoriAPI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Shikimori_API_NET
+{
+    public class ShikimoriAPI
+    {
+        // TODO перенести в конфиг
+        private static string DomenApi { get { return "https://shikimori.one/api/"; } }
+
+
+
+    }
+}
